922cc41053bef3a543190668801b05a97f6fe210
Yair Ken <yairkenn@gmail.com>
2025-02-08 16:07:02 +0200

    changed back to main

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -16,22 +16,8 @@ export const Hero = () => {
         </p>
         <div className="start-btn mx-auto w-fit pt-12 ">
           <MainButton content={"Get your plan"} padding="14px 48px" />
-          <h1>hello</h1>
         </div>
-        <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
-      <div className="relative main md:w-2/3 mx-auto">
-        <h2 className="text-6xl md:text-8xl text-white text-center font-bebas tracking-wide pt-12 md:pt-28">
-          Plan Smarter <span className="text-[#00FF00]">Achieve More</span>
-        </h2>
-        <p className="text-white font-montserrat text-2xl w-3/4 md:w-full font-semibold md:font-normal mx-auto pt-4 md:text-3xl text-center tracking-wider">
-          Unlock your full potential with personalized fitness plans powered by
-          advanced AI technology. Stay on track, push your limits.
-        </p>
-        <div className="start-btn mx-auto w-fit pt-12 ">
-          <MainButton content={"Get your plan"} padding="14px 48px" />
-          <h1>hello</h1>
-        </div>
-      </div>
+      </div>  
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import 'dotenv/config';
 
 const app = express();
 const PORT = 3000;
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run(){
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = "need your help building a training plan for me at the gym, I have 4 days a week to train, devide it to upper-lower workouts and send a json back devided by group of muslces";
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+
+run();
 
 app.use(cors());
 
